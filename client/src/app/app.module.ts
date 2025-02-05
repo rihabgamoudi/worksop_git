2ad0d2adf6a89efbd664ff530955918200e459ad
rihab <riheb.gammoudi@isimg.tn>
2025-02-05 20:51:32 +0100

    ex1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ex1Component } from './ex1/ex1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ex1Component
   ],
   imports: [
     BrowserModule,
